4f568e10dfae95584ad5f58b62f70a351ae86948
Sverrir Pall Sverrisson <sverrirps@gmail.com>
2015-12-09 15:35:28 +0000

    Added winning test for right column.

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -138,6 +138,21 @@ module.exports = function tictactoeCommandHandler(events) {
         }]
       }
 
+      if((gameState.board[0][2] == cmd.side && gameState.board[1][2] == cmd.side && cmd.x == 2 && cmd.y == 2)
+      || (gameState.board[1][2] == cmd.side && gameState.board[2][2] == cmd.side && cmd.x == 0 && cmd.y == 2)
+      || (gameState.board[2][2] == cmd.side && gameState.board[0][2] == cmd.side && cmd.x == 1 && cmd.y == 2)){
+        return [{
+          id: cmd.id,
+          event: cmd.side + " won, with three in right column",
+          userName: cmd.userName,
+          name:gameState.gameCreatedEvent.name,
+          x:cmd.x,
+          y:cmd.y,
+          side:cmd.side,
+          timeStamp: cmd.timeStamp
+        }]
+      }
+
 
       return [{
         id: cmd.id,
