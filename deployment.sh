7d10b0e60adb540ca31d4d5c5568dbc110c68d41
Sverrir Pall Sverrisson <sverrirps@gmail.com>
2015-12-14 12:33:29 +0000

    changed the port for the test machine

--- deployment.sh
+++ deployment.sh
@@ -5,5 +5,5 @@ ssh vagrant@192.168.33.10 '
 docker stop tictactoeContainer
 docker rm tictactoeContainer
 docker pull sverrirps/tictactoe
-docker run -p 8080:8080 -d --name="tictactoeContainer" -e "NODE_ENV=production" sverrirps/tictactoe 
+docker run -p 9000:8080 -d --name="tictactoeContainer" -e "NODE_ENV=production" sverrirps/tictactoe 
 '
